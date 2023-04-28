14a0092c9fccf6e6e496d07409d9e47103cef25f
Nikhil <nikhilsingh0926@gmail.com>
2023-04-28 16:43:30 +0530

    Styled different components

--- portfoilo/src/Components/topbar/Topbar.jsx
+++ portfoilo/src/Components/topbar/Topbar.jsx
@@ -3,7 +3,16 @@
  export default function Topbar() {
    return (
      <div className="topbar">
-       
+       <div className="wrapper">
+           <div className="left">
+            <a href="#intro" className="logo">genius.</a>
+            <div className="itemContainer">
+                 
+            </div>
+           </div>
+           <div className="right"> 
+           </div>
+       </div>
      </div>
    )
  }
