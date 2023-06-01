ce042840b3f269666aca98bed4b0ed4de9c50a8a
Nikhil <nikhilsingh0926@gmail.com>
2023-06-02 00:30:46 +0530

    Text_animation_added

--- portfoilo/src/App.jsx
+++ portfoilo/src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss"
 import Topbar from "./Components/topbar/Topbar";
-import React from "react";
+// import React from "react";
 import Intro from "./Components/intro/Intro";
 import Portfolio from "./Components/portfolio/Portfolio";
 import Works from "./Components/works/Works";
--- portfoilo/src/Components/intro/Intro.jsx
+++ portfoilo/src/Components/intro/Intro.jsx
@@ -1,22 +1,38 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import {useEffect,useRef} from "react"
+
 export default function Intro() {
+
+
+  const textRef = useRef();
+  useEffect(()=>{
+    init(textRef.current,
+       { showCursor: true,
+          backDelay:500,
+          backSpeed:60,
+         strings: ["Frontend", "Developer","Content creator"],
+       });
+  },[]);
+
+
   return (
     <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/Photo.jpg" alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Hi There, I'm</h2>
-          <h1>Nikhil Maurya</h1>
-          <h3>Software Engineer<span>(Frontend)</span></h3>
-        </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt=""/>
-        </a>
+    <div className="left">
+      <div className="imgContainer">
+        <img src="assets/Photo.jpg" alt="" />
       </div>
     </div>
-  )
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>Nikhil Maurya</h1>
+        <h3>Software Engineer <span ref={textRef} ></span></h3>
+      </div>
+      <a href="#portfolio">
+        <img src="assets/down.png" alt=""/>
+      </a>
+    </div>
+  </div>
+  );
 }
