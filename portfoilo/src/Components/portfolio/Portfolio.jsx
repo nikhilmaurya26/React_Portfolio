65d3e9d3644a9c42ba33122a16a9dbe421226e71
Nikhil <nikhilsingh0926@gmail.com>
2023-07-13 22:32:48 +0530

    Subject- Portfolio page -added portfolio -portfolio css

--- portfoilo/src/Components/portfolio/Portfolio.jsx
+++ portfoilo/src/Components/portfolio/Portfolio.jsx
@@ -1,9 +1,64 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
+import Weather from "../../assets/weather.png";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-    
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList title={item.title} />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+        <div className="items">
+          <img src={Weather} alt="" />
+          <h3>React weather app</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ portfoilo/src/Components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,4 @@
+import "./portfolioList.scss";
+export default function PortfolioList({ title }) {
+  return <li className="portfolioList">{title}</li>;
+}
