4491d3f1ca94936abf9294e405d339e06680c3e0
Nikhil <nikhilsingh0926@gmail.com>
2023-07-13 23:58:56 +0530

    Subject: Portfolio page -Completed portfolio -Created dummy data

--- portfoilo/src/Components/portfolio/Portfolio.jsx
+++ portfoilo/src/Components/portfolio/Portfolio.jsx
@@ -1,8 +1,19 @@
 import "./portfolio.scss";
-import Weather from "../../assets/weather.png";
+// import Weather from "../../assets/weather.png";
 import PortfolioList from "../portfolioList/PortfolioList";
+import { useEffect, useState } from "react";
+// import { ListItemSecondaryAction } from "@material-ui/core";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
   const list = [
     {
       id: "featured",
@@ -25,39 +36,48 @@ export default function Portfolio() {
       title: "Content",
     },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
         {list.map((item) => (
-          <PortfolioList title={item.title} />
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
         ))}
       </ul>
       <div className="container">
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
-        <div className="items">
-          <img src={Weather} alt="" />
-          <h3>React weather app</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ portfoilo/src/data.jsx
@@ -0,0 +1,114 @@
+import Weather from "./assets/weather.png";
+import mobile from "./assets/mobile.jpeg"
+
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 2,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 3,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 4,
+    title: "Social media App",
+    img: Weather,
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Social media App",
+    img: mobile,
+  },
+  {
+    id: 2,
+    title: "Social media App",
+    img: mobile,
+  },
+  {
+    id: 3,
+    title: "Social media App",
+    img: mobile,
+  },
+  {
+    id: 4,
+    title: "Social media App",
+    img: mobile,
+  },
+];
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 2,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 3,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 4,
+    title: "Social media App",
+    img: Weather,
+  },
+];
+export const designPortfolio = [
+  {
+    id: 1,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 2,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 3,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 4,
+    title: "Social media App",
+    img: Weather,
+  },
+];
+export const contentPortfolio = [
+  {
+    id: 1,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 2,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 3,
+    title: "Social media App",
+    img: Weather,
+  },
+  {
+    id: 4,
+    title: "Social media App",
+    img: Weather,
+  },
+];
